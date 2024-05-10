9add55af49d09ffd09b7b40d457a1d8ab78694bb
Emmanuel <emmanuel.soes.br@gmail.com>
2024-05-10 20:53:29 -0300

    player update

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed = 1f;
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        if (Input.GetKey(KeyCode.W))    // Movimento preciso
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
         
+        float horizontal = Input.GetAxis("Horizontal");     // Movimento suave (aceleracao)
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime);  
+             
     }
 }
