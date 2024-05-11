bf334965e5bdd6ec88d80779075a6fb75f16d940
Emmanuel <emmanuel.soes.br@gmail.com>
2024-05-10 21:25:58 -0300

    player movement update

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -6,31 +6,37 @@ public class PlayerBehavior : MonoBehaviour
 {
     [SerializeField] private float speed = 1f;
 
+    // Public pois sao variaveis temporarias para testes
+    public bool preciseMovement = true;
+
     private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.W))    // Movimento preciso
+        if (preciseMovement)    // Movimento preciso (sem aceleracao)
         {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.back * speed * Time.deltaTime);
-        }
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.back * speed * Time.deltaTime);
+            }
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+            }
         }
-        else if (Input.GetKey(KeyCode.D))
+        else    // Movimento suave (com aceleracao)
         {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            transform.Translate(new Vector3(horizontal, 0f, vertical) * speed * 1.3f * Time.deltaTime);
         }
-
-        
-        float horizontal = Input.GetAxis("Horizontal");     // Movimento suave (aceleracao)
-        float vertical = Input.GetAxis("Vertical");
-
-        transform.Translate(new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime);  
-             
     }
 }
