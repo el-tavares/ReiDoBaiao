d9343f9bd14976f8f5f5e5ea897a3e50b1e8a359
Emmanuel <emmanuel.soes.br@gmail.com>
2024-05-11 23:12:57 -0300

    songbar update

--- Assets/Scripts/SongbarBehavior.cs
+++ Assets/Scripts/SongbarBehavior.cs
@@ -10,25 +10,47 @@ public class SongbarBehavior : MonoBehaviour
 
     [SerializeField] private float speed = 1f;
     private float direction = 1f;
+    private bool bPressing;
     private bool bHit;
+    private bool bOpened = true;
 
-    private void FixedUpdate()
+    private void Start()
+    {
+        hitbar.value = Random.Range(0f, 1f);    // Escolhe posicao randomica do hitbar
+    }
+
+    private void Update()
     {
         // Anda pra direita se direcao for 1 e valor da barra nao chega em 1. Anda pra esquerda se direcao for -1 e valor da barra nao chega em 0
         if (handlebar.value < 1f && direction > 0) { handlebar.value += speed * direction * Time.deltaTime; }
         else if (handlebar.value > 0f && direction < 0) { handlebar.value += speed * direction * Time.deltaTime; }
         else { direction *= -1f; }  // Troca de direcao quado a barra atingir os valores
+
+        if (!bHit && bOpened)   // Checa se barra de espaco for pressionado ou nao se nao tiver acertado e disponivel
+        {
+            if (Input.GetKey(KeyCode.Space)) { bPressing = true; }  
+            else { bPressing = false; }
+        }    
+
+        if (Input.GetKeyUp(KeyCode.Space)) { bOpened = true; }  // Torna disponivel depois que solta a barra de espaco
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (!bHit)  // Checa se colidiu e a barra de espaco esta pressionado enquanto nao acerta
+        if (!bHit && bOpened)  // Checa se colidiu e a barra de espaco esta pressionado enquanto nao acerta
         {
-            if (other.CompareTag("Hit") && Input.GetKey(KeyCode.Space))
-            {
-                Debug.Log("Trigger");
-                bHit = true;    // Fecha a checagem para garantir que rode esse metodo uma vez
+            if (other.CompareTag("Hit") && bPressing)
+            {   
+                Debug.Log("Acertou");
+                hitbar.value = Random.Range(0f, 1f);    // Escolhe posicao randomica do hitbar
+                bOpened = false;                        // Fecha disponibilidade
+                bHit = true;                            // Fecha a checagem para garantir que rode esse metodo uma vez
             }
-        }      
+        }    
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (bHit) { bHit = false; }     // Reinicia hit como nao acertado    
     }
 }
